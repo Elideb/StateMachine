e64110dd6b455378992e05aecf5978fcb66b20a4
Elideb <elorahranma@gmail.com>
2015-01-29 14:24:40 +0100

    Cleaned some using lines.

--- StateMachine/Example/Entity.cs
+++ StateMachine/Example/Entity.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace StateMachine.Example {
 
     class Entity : IStateful<Entity> {
--- StateMachine/Example/EntityStates.cs
+++ StateMachine/Example/EntityStates.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace StateMachine.Example {
 
     static class EntityStates {
